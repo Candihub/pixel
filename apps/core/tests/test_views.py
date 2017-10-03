3fff9c2fc6ba230997fc002245d6e4ac87823fe5
Julien Maupetit <julien@tailordev.fr>
2017-10-03 22:30:41 +0200

    Add smoke test for the HomeView

---
+++ apps/core/tests/test_views.py
@@ -0,0 +1,18 @@
+import pytest
+
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+
+class ContactViewTestCase(TestCase):
+
+    def setUp(self):
+
+        self.url = reverse('core:home')
+
+    def test_renders_the_home_view(self):
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/home.html')
