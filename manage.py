2a12751f472a2662e5ba8fd5302a337a352cf99d
Julien Maupetit <julien@tailordev.fr>
2017-10-03 22:40:05 +0200

    Configure django-configuration (sic!)
    - Add Base and Development settings
    - Use Development settings by default (wsgi + management script)

--- manage.py
+++ manage.py
@@ -3,9 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pixel.settings")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pixel.settings')
+    os.environ.setdefault('DJANGO_CONFIGURATION', 'Development')
     try:
-        from django.core.management import execute_from_command_line
+        from configurations.management import execute_from_command_line  # noqa
     except ImportError:
         # The above import may fail for some other reason. Ensure that the
         # issue is really that Django is missing to avoid masking other
