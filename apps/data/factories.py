282f8eb0cbfac01efa186e166f03d53dd004484c
Julien Maupetit <julien@tailordev.fr>
2017-10-11 16:46:58 +0200

    Add the EntryFactory

--- apps/data/factories.py
+++ apps/data/factories.py
@@ -1,4 +1,4 @@
-from factory import Faker
+from factory import Faker, Sequence, SubFactory
 from factory.django import DjangoModelFactory
 
 
@@ -10,3 +10,15 @@ class RepositoryFactory(DjangoModelFactory):
     class Meta:
         model = 'data.Repository'
         django_get_or_create = ('name', )
+
+
+class EntryFactory(DjangoModelFactory):
+
+    identifier = Sequence(lambda n: 'FK-%06d'.format(n))
+    description = Faker('text', max_nb_chars=300)
+    url = Faker('url')
+    repository = SubFactory(RepositoryFactory)
+
+    class Meta:
+        model = 'data.Entry'
+        django_get_or_create = ('identifier', )
--- apps/data/tests/test_factories.py
+++ apps/data/tests/test_factories.py
@@ -15,3 +15,23 @@ class RepositoryFactoryTestCase(TestCase):
         self.assertGreater(len(repository.name), 1)
         self.assertGreater(len(repository.url), 1)
         self.assertEqual(qs.count(), 1)
+
+
+class EntryFactoryTestCase(TestCase):
+
+    def test_can_create_entry(self):
+
+        entry_qs = models.Entry.objects.all()
+        repository_qs = models.Repository.objects.all()
+
+        self.assertEqual(entry_qs.count(), 0)
+        self.assertEqual(repository_qs.count(), 0)
+
+        entry = factories.EntryFactory()
+
+        self.assertGreater(len(entry.identifier), 1)
+        self.assertGreater(len(entry.description), 1)
+        self.assertGreater(len(entry.url), 1)
+        self.assertGreater(len(entry.repository.name), 1)
+        self.assertEqual(entry_qs.count(), 1)
+        self.assertEqual(repository_qs.count(), 1)
