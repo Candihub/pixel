0d24b05b427405b4d23d5b05f12d35f037e346b5
Julien Maupetit <julien@tailordev.fr>
2017-10-12 08:08:22 +0200

    Add unit tests for the Strain model

--- apps/core/tests/test_models.py
+++ apps/core/tests/test_models.py
@@ -1,3 +1,7 @@
+import datetime
+
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError, transaction
 from django.test import TestCase
 
 from apps.data.factories import EntryFactory
@@ -54,3 +58,68 @@ class SpeciesTestCase(TestCase):
         )
 
         self.assertEqual(str(species), name)
+
+
+class StrainTestCase(TestCase):
+
+    def setUp(self):
+
+        self.species_name = 'Saccharomyces cerevisiae'
+        self.species_reference = EntryFactory()
+        self.species_description = 'lorem ipsum'
+        self.species = models.Species.objects.create(
+            name=self.species_name,
+            reference=self.species_reference,
+            description=self.species_description
+        )
+
+    def test_can_create_strain(self):
+
+        name = 'S288c / XJ24-24a'
+        description = 'lorem ipsum'
+
+        qs = models.Strain.objects.all()
+        self.assertEqual(qs.count(), 0)
+
+        strain = models.Strain.objects.create(
+            name=name,
+            description=description,
+            species=self.species
+        )
+
+        self.assertEqual(strain.name, name)
+        self.assertEqual(strain.description, description)
+        self.assertEqual(strain.species.id, self.species.id)
+        self.assertEqual(qs.count(), 1)
+
+    def test_model_representation(self):
+
+        name = 'S288c / XJ24-24a'
+
+        strain = models.Strain.objects.create(
+            name=name,
+            species=self.species
+        )
+
+        self.assertEqual(str(strain), name)
+
+    def test_cannot_create_two_strains_with_identical_names_for_species(self):
+
+        name = 'S288c / XJ24-24a'
+
+        qs = models.Strain.objects.all()
+        self.assertEqual(qs.count(), 0)
+
+        models.Strain.objects.create(
+            name=name,
+            species=self.species
+        )
+
+        with self.assertRaises(IntegrityError):
+            with transaction.atomic():
+                models.Strain.objects.create(
+                    name=name,
+                    species=self.species
+                )
+
+        self.assertEqual(qs.count(), 1)
