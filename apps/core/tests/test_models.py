bd8e17d5640f042cbcbd1b2e67b6e8a451c9fa26
Julien Maupetit <julien@tailordev.fr>
2017-10-12 08:12:09 +0200

    Add (partial) unit tests for the OmicsUnit model

--- apps/core/tests/test_models.py
+++ apps/core/tests/test_models.py
@@ -136,3 +136,70 @@ class OmicsUnitTypeTestCase(TestCase):
         # TODO
         # raise NotImplementedError('You have work to do @thomasdenecker!')
         pass
+
+
+class OmicsUnitTestCase(TestCase):
+
+    def setUp(self):
+
+        species = models.Species.objects.create(
+            name='Saccharomyces cerevisiae',
+            reference=EntryFactory(),
+            description='lorem ipsum',
+        )
+        self.strain = models.Strain.objects.create(
+            name='S288c / XJ24-24a',
+            species=species,
+        )
+        self.reference = EntryFactory()
+        self.type = models.OmicsUnitType.objects.create(
+            name='Promoter',
+        )
+
+    def test_can_create_omics_unit(self):
+
+        qs = models.OmicsUnit.objects.all()
+        self.assertEqual(qs.count(), 0)
+
+        omics_unit = models.OmicsUnit.objects.create(
+            reference=self.reference,
+            strain=self.strain,
+            type=self.type
+        )
+
+        self.assertEqual(omics_unit.reference.id, self.reference.id)
+        self.assertEqual(omics_unit.strain.id, self.strain.id)
+        self.assertEqual(omics_unit.type.id, self.type.id)
+
+        self.assertEqual(qs.count(), 1)
+
+    def test_omics_unit_default_status(self):
+
+        omics_unit = models.OmicsUnit.objects.create(
+            reference=self.reference,
+            strain=self.strain,
+            type=self.type
+        )
+
+        self.assertEqual(omics_unit.status, models.OmicsUnit.STATUS_DUBIOUS)
+
+    def test_create_omics_unit_with_non_default_status(self):
+
+        omics_unit = models.OmicsUnit.objects.create(
+            reference=self.reference,
+            strain=self.strain,
+            type=self.type,
+            status=models.OmicsUnit.STATUS_INVALID
+        )
+
+        self.assertEqual(omics_unit.status, models.OmicsUnit.STATUS_INVALID)
+
+    def test_model_representation(self):
+        # TODO
+        # raise NotImplementedError('You have work to do @thomasdenecker!')
+        pass
+
+    def test_cannot_create_two_omics_units_with_same_reference_type_and_strain_(self):  # noqa
+        # TODO
+        # raise NotImplementedError('You have work to do @thomasdenecker!')
+        pass
