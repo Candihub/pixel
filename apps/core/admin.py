3701c1a0d3358f2a2a4382502ee22182a66fc460
Thomas Denecker <thomas.denecker@gmail.com>
2017-10-12 16:47:46 +0200

    Register core models for admin

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,1 +1,15 @@
-# Register your models here.
+from django.contrib import admin
+from .models import Species, Strain, OmicsUnitType, \
+                    OmicsUnit, Pixel, Tag, Experiment, \
+                    Analysis, OmicsArea, Pixeler
+
+admin.site.register(Species)
+admin.site.register(Strain)
+admin.site.register(OmicsUnitType)
+admin.site.register(OmicsUnit)
+admin.site.register(Pixel)
+admin.site.register(Tag)
+admin.site.register(Experiment)
+admin.site.register(Analysis)
+admin.site.register(OmicsArea)
+admin.site.register(Pixeler)
