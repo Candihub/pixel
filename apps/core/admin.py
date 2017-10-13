55fe421ef9fe1cba57b035df62d458b526daca47
Thomas Denecker <thomas.denecker@gmail.com>
2017-10-13 17:20:03 +0200

    Add ModelAdmin

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,16 +1,65 @@
 from django.contrib import admin
-from .models import (
-    Analysis, Experiment, OmicsArea, OmicsUnit, OmicsUnitType, Pixel, Pixeler,
-    Species, Strain, Tag
-)
+from . import models
 
-admin.site.register(Species)
-admin.site.register(Strain)
-admin.site.register(OmicsUnitType)
-admin.site.register(OmicsUnit)
-admin.site.register(Pixel)
-admin.site.register(Tag)
-admin.site.register(Experiment)
-admin.site.register(Analysis)
-admin.site.register(OmicsArea)
-admin.site.register(Pixeler)
+
+class AnalysisAdmin(admin.ModelAdmin):
+    pass
+
+
+class ExperimentAdmin(admin.ModelAdmin):
+    pass
+
+
+class OmicsAreaAdmin(admin.ModelAdmin):
+    pass
+
+
+class OmicsUnitAdmin(admin.ModelAdmin):
+    pass
+
+
+class OmicsUnitTypeAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    ordering = ['name']
+    list_display = (
+        'name', 'description'
+    )
+
+
+class PixelAdmin(admin.ModelAdmin):
+    pass
+
+
+class SpeciesAdmin(admin.ModelAdmin):
+    model = models.Species
+    list_display = (
+        'name', 'description', 'reference'
+    )
+    ordering = ['name']
+
+
+class StrainAdmin(admin.ModelAdmin):
+    pass
+
+
+class TagAdmin(admin.ModelAdmin):
+    model = models.Tag
+
+
+class PixelerAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name',
+                    'last_name', 'email', 'last_login'
+                    )
+    list_filter = ('is_superuser', 'is_active', 'is_staff')
+
+
+admin.site.register(models.Analysis, AnalysisAdmin)
+admin.site.register(models.Experiment, ExperimentAdmin)
+admin.site.register(models.OmicsArea, OmicsAreaAdmin)
+admin.site.register(models.OmicsUnit, OmicsUnitAdmin)
+admin.site.register(models.OmicsUnitType, OmicsUnitTypeAdmin)
+admin.site.register(models.Pixel, PixelAdmin)
+admin.site.register(models.Pixeler, PixelerAdmin)
+admin.site.register(models.Species, SpeciesAdmin)
+admin.site.register(models.Strain, StrainAdmin)
+admin.site.register(models.Tag, TagAdmin)
--- apps/data/admin.py
+++ apps/data/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from .models import Entry, Repository
 
-admin.site.register(Repository)
-admin.site.register(Entry)
+
+@admin.register(Repository)
+class RepositoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    ordering = ['name']
+    list_display = (
+        'name', 'url'
+    )
+
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    search_fields = ['identifier', 'repository']
+    list_display = (
+        'identifier', 'description', 'url', 'repository_url'
+    )
+
+    def repository_url(self, obj):
+        return '<a href="%s" target="_blank">%s</a>' % \
+                (obj.repository.url, obj.repository.name)
+    repository_url.short_description = 'Repository'
+    repository_url.allow_tags = True
