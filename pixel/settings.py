76ba5e3f1f060ee3078e35a3e58697a8bd8e2a34
Julien Maupetit <julien@tailordev.fr>
2017-10-25 23:40:21 +0200

    Configure media/statics paths & urls

--- pixel/settings.py
+++ pixel/settings.py
@@ -125,11 +125,13 @@ class Base(Configuration):
     USE_TZ = True
 
     # Static files (CSS, JavaScript, Images)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
     STATIC_URL = '/static/'
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
     ]
-
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'public', 'media')
+    MEDIA_URL = '/media/'
 
 class Development(Base):
 
