874685b9634d0ad9db773c790a17c891dc82b99d
Julien Maupetit <julien@tailordev.fr>
2017-11-10 21:06:54 +0100

    Fix urls style

--- apps/submission/urls.py
+++ apps/submission/urls.py
@@ -3,6 +3,14 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^download/$', views.DownloadXLSXTemplateView.as_view(), name='download'),
-    url(r'^download/template$', views.GenerateXLSXTemplateView.as_view(), name='generate_template'),
+    url(
+        r'^download/$',
+        views.DownloadXLSXTemplateView.as_view(),
+        name='download'
+    ),
+    url(
+        r'^download/template$',
+        views.GenerateXLSXTemplateView.as_view(),
+        name='generate_template'
+    ),
 ]
