5e3eb3c3a7edaa70d6660bc80c1a13e9b9410f1d
Julien Maupetit <julien@tailordev.fr>
2017-12-11 14:08:08 +0100

    Add hide_traceback template tag filter & use it

---
+++ apps/submission/templatetags/submission.py
@@ -0,0 +1,14 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+@register.filter
+@stringfilter
+def hide_traceback(value):
+    lines = value.split('\n')
+    try:
+        idx = lines.index('Traceback (most recent call last):')
+    except ValueError:
+        return value
+    return lines[:idx][0]
