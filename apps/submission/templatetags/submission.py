6bfb400b24fb791f66f04dc9dee3c936496fe499
Julien Maupetit <julien@tailordev.fr>
2017-12-11 14:50:12 +0100

    Add docs and improve submission template tags

--- apps/submission/templatetags/submission.py
+++ apps/submission/templatetags/submission.py
@@ -6,14 +6,23 @@ register = template.Library()
 @register.filter
 @stringfilter
 def hide_traceback(value):
+    """Removes traceback lines from a string (if any). It has no effect when
+    no 'Traceback' pattern has been found.
+
+    Returns: raws before the 'Traceback' pattern
+    """
     lines = value.split('\n')
     try:
         idx = lines.index('Traceback (most recent call last):')
     except ValueError:
         return value
-    return lines[:idx][0]
+    return '\n'.join(lines[:idx])
 
 
 @register.filter
 def hide_check_tasks(tasks):
+    """Remove a task from a task queryset if it's class name starts by 'check'.
+
+    Returns: a filtered list of tasks
+    """
     return [t for t in tasks if 'check' not in str(t).lower()]
