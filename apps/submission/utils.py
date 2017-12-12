63e48d07d4f80bf28bbee8cac2b9bcd02c821496
Julien Maupetit <julien@tailordev.fr>
2017-12-12 14:13:04 +0100

    Add tests for submission utils

--- apps/submission/utils.py
+++ apps/submission/utils.py
@@ -1,7 +1,11 @@
 import re
 
+from django.utils.translation import ugettext as _
+
 
 def is_hidden_task(task_name):
+    if not isinstance(task_name, str):
+        raise ValueError(_("Task name should be a string"))
     if re.match('start|end|check', task_name):
         return True
     return False
