0b18445f7ba9fba1f5623e0306cf090d96a6f4cb
Julien Maupetit <julien@tailordev.fr>
2017-12-18 16:03:14 +0100

    Add save() method to PixelSetParser

--- apps/submission/io/pixel.py
+++ apps/submission/io/pixel.py
@@ -1,12 +1,32 @@
 import pandas
 
+from django.utils.translation import ugettext as _
+
+from apps.core.models import OmicsUnit, Pixel, PixelSet
+from apps.data.models import Entry
+from ..exceptions import PixelSetParserError, PixelSetParserSaveError
+
 
 class PixelSetParser(object):
 
-    def __init__(self, pixelset_path):
+    def __init__(self,
+                 pixelset_path,
+                 description=None,
+                 analysis=None,
+                 omics_unit_type=None,
+                 strain=None):
 
+        self.pixelset = None
         self.pixelset_path = pixelset_path
+        self.description = description
+        self.analysis = analysis
+        self.omics_unit_type = omics_unit_type
+        self.strain = strain
         self.pixels = None
+        self.db_pixels = {
+            'new': [],
+            'update': [],
+        }
 
     def parse(self, force=False):
 
@@ -35,3 +55,127 @@ class PixelSetParser(object):
             pixels = pixels.loc[pixels.index.drop(fuzzy.index), :]
 
         return pixels, na, fuzzy
+
+    def _set_pixel_set(self):
+
+        if self.analysis is None:
+            raise PixelSetParserError(
+                _(
+                    "You need to define PixelSetParser.analysis to get or "
+                    "create a PixelSet"
+                )
+            )
+
+        self.pixelset, __ = PixelSet.objects.get_or_create(
+            pixels_file__exact=self.pixelset_path,
+            description=self.description,
+            analysis=self.analysis,
+        )
+        # Get or create cannot set file path as is
+        self.pixelset.pixels_file.name = self.pixelset_path
+        self.pixelset.save()
+
+    def _get_omics_units(self, pixels, verbose=False):
+
+        if any((self.strain is None, self.omics_unit_type is None)):
+            raise PixelSetParserSaveError(
+                _(
+                    "You need to define PixelSetParser.strain and "
+                    "PixelSetParser.omics_unit_type to get omics units"
+                )
+            )
+
+        identifiers = pixels.axes[0].tolist()
+        existing = OmicsUnit.objects.filter(
+            strain=self.strain,
+            type=self.omics_unit_type
+        ).values_list('reference__identifier', flat=True)
+
+        to_create = [
+            p for p in identifiers if p not in existing
+        ]
+
+        related_entries = Entry.objects.filter(identifier__in=to_create)
+
+        if related_entries.count() != len(to_create):
+            ids = list(related_entries.values_list('identifier', flat=True))
+            raise PixelSetParserSaveError(
+                _(
+                    "Required entries partially exists ({} vs {}). Please "
+                    "load entries first thanks to the load_entries management "
+                    "command."
+                ).format(
+                    related_entries.count(),
+                    len(to_create)
+                )
+            )
+
+        omics_units = []
+        for entry in related_entries:
+            omics_units.append(
+                OmicsUnit(
+                    reference=entry,
+                    strain=self.strain,
+                    type=self.omics_unit_type,
+                )
+            )
+        OmicsUnit.objects.bulk_create(omics_units)
+
+        return OmicsUnit.objects.filter(
+            reference__identifier__in=identifiers
+        )
+
+    def _to_pixels(self):
+
+        if self.pixels is None:
+            return
+
+        self._set_pixel_set()
+        pixels, na, fuzzy = self.filter()
+
+        # Pixels and OmicsUnits are sorted by OmicsUnit reference identifier
+        omics_units = self._get_omics_units(pixels)
+
+        # Look for existing Pixels
+        identifiers = pixels.axes[0].tolist()
+        existing = Pixel.objects.filter(
+            omics_unit__reference__identifier__in=identifiers,
+            pixel_set=self.pixelset,
+        ).values_list(
+            'omics_unit__reference__identifier',
+            flat=True
+        )
+
+        for omics_unit in omics_units:
+            identifier = omics_unit.reference.identifier
+            pixel = pixels.ix[identifier]
+            db_pixel = Pixel(
+                value=pixel.Value,
+                quality_score=pixel.Quality_score,
+                omics_unit=omics_unit,
+                pixel_set=self.pixelset,
+            )
+            if identifier in existing:
+                self.db_pixels['update'].append(db_pixel)
+            else:
+                self.db_pixels['new'].append(db_pixel)
+
+    def save(self, update=False):
+
+        self._to_pixels()
+
+        # Create news entries
+        Pixel.objects.bulk_create(self.db_pixels['new'], batch_size=500)
+
+        if not update:
+            return
+
+        # Update old entries
+        for updated_pixel in self.db_pixels['update']:
+            pixel = Pixel.objects.get(
+                omics_unit=updated_pixel.omics_unit,
+                pixel_set=updated_pixel.pixel_set
+            )
+            pixel.value = updated_pixel.value
+            pixel.quality_score = updated_pixel.quality_score
+            pixel.save(update_fields=['value', 'quality_score'])
--- apps/submission/tests/io/test_pixel.py
+++ apps/submission/tests/io/test_pixel.py
@@ -2,7 +2,16 @@ from pathlib import Path
 
 from django.test import TestCase
 
+from apps.core.factories import (
+    AnalysisFactory, OmicsUnitFactory, OmicsUnitTypeFactory, PixelFactory,
+    StrainFactory
+)
+from apps.core.models import OmicsUnit, Pixel
+from apps.data.factories import EntryFactory
+from apps.data.io.cgd import ChrFeatureParser
+from apps.data.models import Repository
 from apps.submission.io.pixel import PixelSetParser
+from ...exceptions import PixelSetParserError, PixelSetParserSaveError
 
 
 class PixelTestCase(TestCase):
@@ -11,6 +20,46 @@ class PixelTestCase(TestCase):
 
         self.pixelset_path = Path(
             'apps/submission/fixtures/dataset-0001/Pixel_C10.txt'
+        )
+        self.cgd_path = Path(
+            'apps/submission/fixtures/'
+        ) / Path(
+            'C_glabrata_CBS138_current_chromosomal_feature_required.tab'
+        )
+        self.analysis = AnalysisFactory()
+        self.description = "lorem ipsum"
+        self.omics_unit_type = OmicsUnitTypeFactory()
+        self.strain = StrainFactory()
+
+    def _load_cgd_entries(self):
+
+        cgd_parser = ChrFeatureParser(self.cgd_path)
+        cgd_parser.parse()
+        cgd_parser.save(ignore_aliases=False)
+
+    def _create_pixel_from_set(self, pixelset):
+        entry = EntryFactory(
+            identifier='CAGL0K09834g',
+            description=(
+                'Ortholog(s) have role in response to salt stress and '
+                'cytoplasm localization'
+            ),
+            url=(
+                'http://www.candidagenome.org/cgi-bin/locus.pl?dbid=/'
+                'CAL0134577'
+            ),
+            repository=Repository.objects.get(name='CGD')
+        )
+        omics_unit = OmicsUnitFactory(
+            reference=entry,
+            strain=self.strain,
+            type=self.omics_unit_type,
+        )
+        return PixelFactory(
+            value=4.2,
+            quality_score=0.8,
+            omics_unit=omics_unit,
+            pixel_set=pixelset,
         )
 
     def test_init(self):
@@ -87,3 +136,191 @@ class PixelTestCase(TestCase):
         self.assertEqual(len(pixels), 1862)
         self.assertEqual(pixels.index[0], 'CAGL0F02695g')
         self.assertEqual(pixels.index[-1], 'CAGL0L01925g')
+
+    def test__set_pixel_set(self):
+
+        parser = PixelSetParser(self.pixelset_path)
+        with self.assertRaises(PixelSetParserError):
+            parser._set_pixel_set()
+
+        parser = PixelSetParser(
+            self.pixelset_path,
+            description=self.description,
+            analysis=self.analysis,
+        )
+        parser._set_pixel_set()
+        pixelset = parser.pixelset
+        self.assertIsNotNone(pixelset)
+        self.assertEqual(pixelset.pixels_file.name, self.pixelset_path)
+        self.assertEqual(pixelset.description, self.description)
+        self.assertEqual(pixelset.analysis, self.analysis)
+
+    def test__get_omics_units(self):
+
+        parser = PixelSetParser(
+            self.pixelset_path,
+            description=self.description,
+            analysis=self.analysis,
+        )
+        with self.assertRaises(PixelSetParserSaveError):
+            parser._get_omics_units(None)
+
+        parser = PixelSetParser(
+            self.pixelset_path,
+            description=self.description,
+            analysis=self.analysis,
+            strain=self.strain,
+        )
+        with self.assertRaises(PixelSetParserSaveError):
+            parser._get_omics_units(None)
+
+        parser = PixelSetParser(
+            self.pixelset_path,
+            description=self.description,
+            analysis=self.analysis,
+            omics_unit_type=self.omics_unit_type,
+        )
+        with self.assertRaises(PixelSetParserSaveError):
+            parser._get_omics_units(None)
+
+        parser = PixelSetParser(
+            self.pixelset_path,
+            description=self.description,
+            analysis=self.analysis,
+            omics_unit_type=self.omics_unit_type,
+            strain=self.strain,
+        )
+        parser.parse()
+        pixels, _, _ = parser.filter()
+
+        # We need to load Entries first!
+        self.assertEqual(OmicsUnit.objects.count(), 0)
+        with self.assertRaises(PixelSetParserSaveError):
+            parser._get_omics_units(pixels)
+        self.assertEqual(OmicsUnit.objects.count(), 0)
+
+        # So… load entries
+        self._load_cgd_entries()
+
+        # Then create omics units
+        omics_units = parser._get_omics_units(pixels, verbose=True)
+        self.assertEqual(omics_units.count(), 1837)
+        self.assertEqual(OmicsUnit.objects.count(), 1837)
+
+    def test__to_pixels(self):
+
+        parser = PixelSetParser(
+            self.pixelset_path,
+            description=self.description,
+            analysis=self.analysis,
+            omics_unit_type=self.omics_unit_type,
+            strain=self.strain,
+        )
+
+        # With no pixel file parsed, _to_pixels has no effect
+        parser._to_pixels()
+        self.assertEqual(
+            parser.db_pixels,
+            {
+                'new': [],
+                'update': [],
+            }
+        )
+
+        parser.parse()
+        self._load_cgd_entries()
+        parser._to_pixels()
+        self.assertEqual(len(parser.db_pixels['new']), 1837)
+        self.assertEqual(len(parser.db_pixels['update']), 0)
+
+        db_pixel = parser.db_pixels['new'][0]
+        pixel = parser.pixels.ix[db_pixel.omics_unit.reference.identifier]
+
+        self.assertEqual(db_pixel.value, pixel.Value)
+        self.assertEqual(db_pixel.quality_score, pixel.Quality_score)
+
+    def test__to_pixels_with_existing_pixel(self):
+
+        parser = PixelSetParser(
+            self.pixelset_path,
+            description=self.description,
+            analysis=self.analysis,
+            omics_unit_type=self.omics_unit_type,
+            strain=self.strain,
+        )
+
+        parser.parse()
+        parser._set_pixel_set()
+        self._load_cgd_entries()
+
+        existing_db_pixel = self._create_pixel_from_set(parser.pixelset)
+
+        parser._to_pixels()
+        self.assertEqual(len(parser.db_pixels['new']), 1836)
+        self.assertEqual(len(parser.db_pixels['update']), 1)
+
+        # We should have updated existing pixel data
+        identifier = existing_db_pixel.omics_unit.reference.identifier
+        pixel = parser.pixels.ix[identifier]
+        updated_db_pixel = parser.db_pixels['update'][0]
+        self.assertEqual(updated_db_pixel.value, pixel.Value)
+        self.assertEqual(updated_db_pixel.quality_score, pixel.Quality_score)
+
+    def test_save(self):
+
+        parser = PixelSetParser(
+            self.pixelset_path,
+            description=self.description,
+            analysis=self.analysis,
+            omics_unit_type=self.omics_unit_type,
+            strain=self.strain,
+        )
+        parser.parse()
+        self._load_cgd_entries()
+
+        self.assertEqual(Pixel.objects.count(), 0)
+        parser.save()
+        self.assertEqual(Pixel.objects.count(), 1837)
+
+    def test_save_with_existing_pixel(self):
+
+        parser = PixelSetParser(
+            self.pixelset_path,
+            description=self.description,
+            analysis=self.analysis,
+            omics_unit_type=self.omics_unit_type,
+            strain=self.strain,
+        )
+        parser.parse()
+        parser._set_pixel_set()
+        self._load_cgd_entries()
+
+        self._create_pixel_from_set(parser.pixelset)
+        self.assertEqual(Pixel.objects.count(), 1)
+        parser.save()
+        self.assertEqual(Pixel.objects.count(), 1837)
+
+    def test_save_with_update(self):
+
+        parser = PixelSetParser(
+            self.pixelset_path,
+            description=self.description,
+            analysis=self.analysis,
+            omics_unit_type=self.omics_unit_type,
+            strain=self.strain,
+        )
+        parser.parse()
+        parser._set_pixel_set()
+        self._load_cgd_entries()
+
+        pixel = self._create_pixel_from_set(parser.pixelset)
+        self.assertEqual(Pixel.objects.count(), 1)
+        self.assertEqual(pixel.value, 4.2)
+        self.assertEqual(pixel.quality_score, 0.8)
+
+        parser.save(update=True)
+        self.assertEqual(Pixel.objects.count(), 1837)
+
+        pixel = Pixel.objects.get(pk=pixel.pk)
+        self.assertEqual(pixel.value, 2.703695974165)
+        self.assertEqual(pixel.quality_score, 0.00268822352590468)
