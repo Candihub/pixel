5532c0660021329114b000ba5599a70b7878d130
Julien Maupetit <julien@tailordev.fr>
2017-12-21 15:55:58 +0100

    Make importation task asynchronous 🚀

--- apps/submission/flows.py
+++ apps/submission/flows.py
@@ -1,15 +1,22 @@
+import logging
+
 from pathlib import Path
 
+import background
+
 from django.conf import settings
-from viewflow import flow
-from viewflow.activation import Context
+from django.utils.timezone import now
+from viewflow import flow, signals as vf_signals
+from viewflow.activation import Context, STATUS
 from viewflow.base import this, Flow
 from viewflow.flow.views import CreateProcessView
 from viewflow.nodes.handler import HandlerActivation
 
-from apps.submission.io.archive import PixelArchive
-from .models import SubmissionProcess
 from . import views
+from .models import SubmissionProcess
+from .io.archive import PixelArchive
+
+logger = logging.getLogger(__name__)
 
 
 class NotPropagatedExceptionHandlerActivation(HandlerActivation):
@@ -18,6 +25,44 @@ class NotPropagatedExceptionHandlerActivation(HandlerActivation):
 
         with Context(propagate_exception=False):
             super().perform()
+
+
+class AsyncActivationHandler(NotPropagatedExceptionHandlerActivation):
+
+    def perform(self):
+
+        with self.exception_guard():
+            self.task.started = now()
+
+            vf_signals.task_started.send(
+                sender=self.flow_class,
+                process=self.process,
+                task=self.task
+            )
+            self.execute()
+
+    def callback(self):
+
+        with Context(propagate_exception=False):
+            with self.exception_guard():
+                logger.debug('Activation class callback started…')
+
+                self.task.finished = now()
+                self.set_status(STATUS.DONE)
+                self.task.save()
+
+                vf_signals.task_finished.send(
+                    sender=self.flow_class,
+                    process=self.process,
+                    task=self.task
+                )
+
+                self.activate_next()
+
+
+class AsyncHandler(flow.Handler):
+
+    activation_class = AsyncActivationHandler
 
 
 class SubmissionFlow(Flow):
@@ -96,9 +141,8 @@ class SubmissionFlow(Flow):
         this.validation
     )
 
-    import_archive = flow.Handler(
+    import_archive = AsyncHandler(
         this.perform_archive_importation,
-        activation_class=NotPropagatedExceptionHandlerActivation,
     ).Next(
         this.check_import
     )
@@ -113,13 +157,16 @@ class SubmissionFlow(Flow):
 
     end = flow.End()
 
-    def parse_meta(self, activation):
-
-        archive_path = Path(
+    def get_archive_path(self, activation):
+        return Path(
             settings.MEDIA_ROOT
         ) / Path(
             activation.process.archive.name
         )
+
+    def parse_meta(self, activation):
+
+        archive_path = self.get_archive_path(activation)
         archive = PixelArchive(archive_path)
         archive.parse(serialized=True)
         activation.process.meta = archive.meta
@@ -127,13 +174,41 @@ class SubmissionFlow(Flow):
 
     def perform_archive_importation(self, activation):
 
-        archive_path = Path(
-            settings.MEDIA_ROOT
-        ) / Path(
-            activation.process.archive.name
-        )
-        archive = PixelArchive(archive_path)
-        archive.save(pixeler=activation.process.created_by)
+        archive_path = self.get_archive_path(activation)
+        process = activation.process
+        pixeler = process.created_by
+        task = process.get_task(self.import_archive)
 
-        activation.process.imported = True
-        activation.process.save()
+        @background.task
+        def async_import_archive():
+            archive = PixelArchive(archive_path)
+            return archive.save(pixeler=pixeler)
+
+        @background.callback
+        def importation_callback(future):
+            logger.debug('Background importation callback started…')
+
+            e = future.exception()
+            if e is not None:
+                task.status = STATUS.ERROR
+                task.comments = str(e)
+                task.finished = now()
+                task.save()
+                try:
+                    raise e
+                except Exception:
+                    logger.exception(
+                        'Importation failed! archive: {} (pixeler: {})'.format(
+                            archive_path,
+                            pixeler
+                        )
+                    )
+                    raise
+
+            process.imported = True
+            process.save()
+            activation.callback()
+
+            logger.debug('Background importation callback done')
+
+        async_import_archive()
