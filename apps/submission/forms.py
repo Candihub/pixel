a4b77d212a9112c6884e341d8e9c28abc174898c
Julien Maupetit <julien@tailordev.fr>
2018-01-15 17:48:05 +0100

    Add support for submission process tags update

--- apps/submission/forms.py
+++ apps/submission/forms.py
@@ -7,19 +7,19 @@ from .models import SubmissionProcess
 
 class SubmissionTagsForm(forms.ModelForm):
 
-    def _get_tags():
+    def _get_existing_tags():
         return [(t.name, t.name) for t in Tag.objects.all()]
 
-    experiment_tags = forms.ChoiceField(
+    experiment_tags = forms.MultipleChoiceField(
         label=_("Select one or more tags"),
-        choices=_get_tags,
+        choices=_get_existing_tags,
         widget=forms.CheckboxSelectMultiple,
         required=False,
     )
 
-    analysis_tags = forms.ChoiceField(
+    analysis_tags = forms.MultipleChoiceField(
         label=_("Select one or more tags"),
-        choices=_get_tags,
+        choices=_get_existing_tags,
         widget=forms.CheckboxSelectMultiple,
         required=False,
     )
@@ -60,3 +60,49 @@ class SubmissionTagsForm(forms.ModelForm):
         widgets = {
             'tags': forms.HiddenInput,
         }
+
+    @staticmethod
+    def _clean_tags(tags):
+        """Clean input tags: sorted, stripped, lowercase and unique"""
+
+        return ','.join(
+            sorted(set(t.lower().strip() for t in tags.split(',')))
+        )
+
+    def _set_tags(self):
+        """Set process tags"""
+
+        if not hasattr(self, 'cleaned_data'):
+            return
+
+        analysis_tags = self._clean_tags(
+            ','.join((
+                ','.join(self.cleaned_data['analysis_tags']),
+                self.cleaned_data['new_analysis_tags'],
+            ))
+        )
+
+        experiment_tags = self._clean_tags(
+            ','.join((
+                ','.join(self.cleaned_data['experiment_tags']),
+                self.cleaned_data['new_experiment_tags'],
+            ))
+        )
+
+        self.instance.tags = self.cleaned_data['tags'] = {
+            'analysis': analysis_tags,
+            'experiment': experiment_tags,
+        }
+
+    def clean_new_experiment_tags(self):
+        return self._clean_tags(self.cleaned_data['new_experiment_tags'])
+
+    def clean_new_analysis_tags(self):
+        return self._clean_tags(self.cleaned_data['new_analysis_tags'])
+
+    def save(self, **kwargs):
+        """Set tags before saving"""
+
+        self._set_tags()
+
+        return super().save(**kwargs)
---
+++ apps/submission/tests/test_forms.py
@@ -0,0 +1,165 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from apps.core.factories import PIXELER_PASSWORD, PixelerFactory
+from apps.core.models import Tag
+from ..forms import SubmissionTagsForm
+from ..models import SubmissionProcess
+
+
+class SubmissionTagsFormTestCase(TestCase):
+
+    def setUp(self):
+
+        # Create submission process
+        user = PixelerFactory(
+            is_active=True,
+            is_staff=True,
+            is_superuser=True,
+        )
+        self.client.login(
+            username=user.username,
+            password=PIXELER_PASSWORD,
+        )
+        self.client.post(
+            reverse('submission:start'),
+            data={
+                'label': 'Candida datasest 0001',
+                '_viewflow_activation-started': '2000-01-01',
+            },
+            follow=True,
+        )
+        self.process = SubmissionProcess.objects.get()
+
+        # Create tags
+        tags = (
+            'complex/molecule/atom',
+            'omics/rna',
+            'omics/dna',
+            'omics/protein',
+            'msms',
+        )
+        for tag in tags:
+            Tag.objects.create(name=tag)
+
+    def test__get_existing_tags(self):
+
+        expected = [
+            ('complex', 'complex'),
+            ('complex/molecule', 'complex/molecule'),
+            ('complex/molecule/atom', 'complex/molecule/atom'),
+            ('msms', 'msms'),
+            ('omics', 'omics'),
+            ('omics/dna', 'omics/dna'),
+            ('omics/protein', 'omics/protein'),
+            ('omics/rna', 'omics/rna'),
+        ]
+        self.assertEqual(SubmissionTagsForm._get_existing_tags(), expected)
+
+    def test__clean_tags(self):
+
+        tags = ''
+        expected = ''
+        self.assertEqual(SubmissionTagsForm._clean_tags(tags), expected)
+
+        tags = ' OMICS/protein, omics, Omics , complex, complex/Molecule  '
+        expected = 'complex,complex/molecule,omics,omics/protein'
+        self.assertEqual(SubmissionTagsForm._clean_tags(tags), expected)
+
+        # unicode, yeah 🎉
+        tags = '🍕, complex/molecule, 🎸'
+        expected = 'complex/molecule,🍕,🎸'
+        self.assertEqual(SubmissionTagsForm._clean_tags(tags), expected)
+
+    def test_clean_new_experiment_tags(self):
+
+        data = {
+            'new_experiment_tags': ' OMICS/protein, omics, complex/Molecule  '
+        }
+        form = SubmissionTagsForm(data)
+        form.is_valid()
+        expected = 'complex/molecule,omics,omics/protein'
+        self.assertEqual(form.clean_new_experiment_tags(), expected)
+
+    def test_clean_new_analysis_tags(self):
+
+        data = {
+            'new_analysis_tags': ' OMICS/protein, omics, complex/Molecule  '
+        }
+        form = SubmissionTagsForm(data)
+        form.is_valid()
+        expected = 'complex/molecule,omics,omics/protein'
+        self.assertEqual(form.clean_new_analysis_tags(), expected)
+
+    def test_clean(self):
+
+        data = {
+            'experiment_tags': ['msms', 'omics/rna'],
+            'analysis_tags': ['complex/molecule/atom', 'msms'],
+            'new_analysis_tags': 'ijm, R',
+            'new_experiment_tags': 'msms/time, rna-seq',
+            'tags': None,
+        }
+
+        form = SubmissionTagsForm(data)
+        assert form.is_valid()
+
+        expected = {
+            'experiment_tags': ['msms', 'omics/rna'],
+            'analysis_tags': ['complex/molecule/atom', 'msms'],
+            'new_analysis_tags': 'ijm,r',
+            'new_experiment_tags': 'msms/time,rna-seq',
+            'tags': None,
+        }
+        self.assertEqual(form.cleaned_data, expected)
+
+    def test__set_tags(self):
+
+        data = {
+            'experiment_tags': ['msms', 'omics/rna'],
+            'analysis_tags': ['complex/molecule/atom', 'msms'],
+            'new_analysis_tags': 'ijm, R',
+            'new_experiment_tags': 'msms/time, rna-seq',
+            'tags': None,
+        }
+        form = SubmissionTagsForm(data)
+
+        # We cannot set tags on an unvalidated form
+        self.assertFalse(hasattr(form, 'cleaned_data'))
+        form._set_tags()
+        self.assertFalse(hasattr(form, 'cleaned_data'))
+        self.assertIsNone(form.instance.tags)
+
+        assert form.is_valid()
+        self.assertIsNone(form.cleaned_data['tags'])
+
+        form._set_tags()
+
+        expected = {
+            'analysis': 'complex/molecule/atom,ijm,msms,r',
+            'experiment': 'msms,msms/time,omics/rna,rna-seq'
+        }
+        self.assertEqual(form.cleaned_data['tags'], expected)
+        self.assertEqual(form.instance.tags, expected)
+
+    def test_save(self):
+
+        self.assertIsNone(self.process.tags)
+
+        data = {
+            'experiment_tags': ['msms', 'omics/rna'],
+            'analysis_tags': ['complex/molecule/atom', 'msms'],
+            'new_analysis_tags': 'ijm, R',
+            'new_experiment_tags': 'msms/time, rna-seq',
+            'tags': {},
+        }
+        form = SubmissionTagsForm(data, instance=self.process)
+        assert form.is_valid()
+
+        expected = {
+            'analysis': 'complex/molecule/atom,ijm,msms,r',
+            'experiment': 'msms,msms/time,omics/rna,rna-seq'
+        }
+        process = form.save()
+        self.assertEqual(process.tags, expected)
+        self.assertEqual(self.process.tags, expected)
