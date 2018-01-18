45e007b90e32852e46de48a44373497f33bbb16e
Julien Maupetit <julien@tailordev.fr>
2018-01-18 11:12:51 +0100

    Add make_development_fixtures command

---
+++ apps/core/management/commands/make_development_fixtures.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.translation import ugettext as _
+
+from apps.core.factories import PixelSetFactory
+
+DEFAULT_N_PIXELSETS = 10
+
+
+def make_development_fixtures(n_pixel_sets=DEFAULT_N_PIXELSETS):
+    """Generate development fixtures"""
+
+    PixelSetFactory.create_batch(n_pixel_sets)
+
+
+class Command(BaseCommand):
+    help = _("Generate development fixtures")
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--pixel-sets',
+            type=int,
+            action='store',
+            default=DEFAULT_N_PIXELSETS,
+            help=_("the number of pixel set(s) to generate")
+        )
+
+    def handle(self, *args, **options):
+
+        if not settings.DEBUG:
+            raise CommandError(
+                _(
+                    "This command should only be used in development, "
+                    "i.e. with DEBUG=True"
+                )
+            )
+
+        n_pixel_sets = options.get('pixel_sets', DEFAULT_N_PIXELSETS)
+
+        self.stdout.write(
+            _("Will generate {} pixel sets…".format(n_pixel_sets))
+        )
+
+        make_development_fixtures(n_pixel_sets=n_pixel_sets)
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                _("Successfully generated fixtures")
+            )
+        )
---
+++ apps/core/tests/test_commands.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+from django.core.management import call_command
+from django.core.management.base import CommandError
+from django.test import TestCase, override_settings
+from django.utils.six import StringIO
+
+from ..management.commands.make_development_fixtures import DEFAULT_N_PIXELSETS
+from ..models import PixelSet
+
+
+@override_settings(DEBUG=True)
+class MakeDevelopmentFixturesCommandTestCase(TestCase):
+
+    def test_command(self):
+
+        output = StringIO()
+
+        with self.settings(DEBUG=False):
+            with self.assertRaises(CommandError):
+                call_command('make_development_fixtures', stdout=output)
+
+        self.assertEqual(PixelSet.objects.count(), 0)
+        call_command('make_development_fixtures', stdout=output)
+        self.assertEqual(PixelSet.objects.count(), DEFAULT_N_PIXELSETS)
+
+        expected_output = (
+            'Will generate {} pixel sets…\n'
+            'Successfully generated fixtures'
+        ).format(DEFAULT_N_PIXELSETS)
+        self.assertIn(expected_output, output.getvalue())
+
+    def test_command_with_custom_pixelset(self):
+
+        output = StringIO()
+
+        n_pixel_sets = 5
+        self.assertEqual(PixelSet.objects.count(), 0)
+        call_command(
+            'make_development_fixtures',
+            pixel_sets=n_pixel_sets,
+            stdout=output
+        )
+        self.assertEqual(PixelSet.objects.count(), n_pixel_sets)
