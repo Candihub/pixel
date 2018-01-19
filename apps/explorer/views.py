e3387a4f5294b188ebb0cc794b6e7c60254c11b8
Julien Maupetit <julien@tailordev.fr>
2018-01-19 11:20:54 +0100

    Add explorer urls and draft pixel sets list view

--- apps/explorer/views.py
+++ apps/explorer/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 
-# Create your views here.
+from apps.core.models import PixelSet
+
+
+class PixelSetListView(LoginRequiredMixin, ListView):
+
+    model = PixelSet
+    template_name = 'explorer/pixelset_list.html'
