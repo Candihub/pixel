a59bf6e5063578e9f50af556c3d025b56aad7e54
Julien Maupetit <julien@tailordev.fr>
2018-01-19 15:17:44 +0100

    Paginate PixelSet list and optimize db queries

--- apps/explorer/views.py
+++ apps/explorer/views.py
@@ -7,4 +7,10 @@ from apps.core.models import PixelSet
 class PixelSetListView(LoginRequiredMixin, ListView):
 
     model = PixelSet
+    paginate_by = 10
+    queryset = PixelSet.objects.all().prefetch_related(
+        'analysis__experiments__omics_area',
+        'pixels__omics_unit__type',
+        'pixels__omics_unit__strain__species',
+    )
     template_name = 'explorer/pixelset_list.html'
