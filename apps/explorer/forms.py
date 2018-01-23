c090ed4b0dfa7c64431bab174e4552e9b7535198
Julien Maupetit <julien@tailordev.fr>
2018-01-23 12:23:43 +0100

    Use ModelMultipleChoiceField when needed

--- apps/explorer/forms.py
+++ apps/explorer/forms.py
@@ -1,43 +1,37 @@
 from django import forms
 from django.utils.translation import ugettext as _
+from mptt.forms import TreeNodeMultipleChoiceField
 
 from apps.core import models
 
 
 class PixelSetFiltersForm(forms.Form):
 
-    def get_species():
-        return ((s.id, s.name) for s in models.Species.objects.all())
-
-    def get_omics_unit_types():
-        return ((o.id, o.name) for o in models.OmicsUnitType.objects.all())
-
-    def get_omics_areas():
-        return ((o.id, o.name) for o in models.OmicsArea.objects.all())
-
-    def get_tags():
-        return ((t.id, t.name) for t in models.Tag.objects.all())
-
-    species = forms.MultipleChoiceField(
+    species = forms.ModelMultipleChoiceField(
         label=_("Species"),
-        choices=get_species(),
+        queryset=models.Species.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
         required=False,
     )
 
-    omics_unit_types = forms.MultipleChoiceField(
+    omics_unit_types = forms.ModelMultipleChoiceField(
         label=_("Omics Unit Types"),
-        choices=get_omics_unit_types(),
+        queryset=models.OmicsUnitType.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
         required=False,
     )
 
-    omics_areas = forms.MultipleChoiceField(
+    omics_areas = TreeNodeMultipleChoiceField(
         label=_("Omics Areas"),
-        choices=get_omics_areas(),
+        queryset=models.OmicsArea.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        level_indicator='—',
         required=False,
     )
 
-    tags = forms.MultipleChoiceField(
+    tags = forms.ModelMultipleChoiceField(
         label=_("Tags"),
-        choices=get_tags(),
+        queryset=models.Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
         required=False,
     )
