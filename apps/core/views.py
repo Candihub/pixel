f562c32acb2aa843c809f59839cd34542e189152
William Durand <will+git@drnd.me>
2018-02-06 12:47:23 +0100

    Refactor the pie chart on the home

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,7 @@
+from django.utils.translation import ugettext as _
 from django.views.generic import TemplateView
 from django.shortcuts import render
+
 
 from apps.core.models import Pixel
 
@@ -12,17 +14,20 @@ class HomeView(TemplateView):
 
         if request.user.is_authenticated():
             pixels_by_species = Pixel.objects.raw((
-                'SELECT core_species.name AS name, count(core_pixel.id) AS nb,'
+                'SELECT core_species.name AS name, count(*) AS nb,'
                 ' 1 as id'  # this is a hack to overcome Django ORM limitations
-                ' FROM core_pixel, core_species, core_omicsunit, core_strain'
-                ' WHERE omics_unit_id = core_omicsunit.id'
-                ' AND core_omicsunit.strain_id = core_strain.id'
-                ' AND core_strain.species_id = core_species.id'
+                ' FROM core_pixel'
+                ' JOIN core_omicsunit ON omics_unit_id = core_omicsunit.id'
+                ' JOIN core_strain ON core_omicsunit.strain_id = core_strain.id'
+                ' JOIN core_species ON core_strain.species_id = core_species.id'
                 ' GROUP BY core_species.name'
             ))
 
             return render(request, 'core/home-authenticated.html', {
-                'pixels_by_species': pixels_by_species,
+                'pixels_by_species': {
+                    'title': _('Pixels by Species'),
+                    'data': [[row.name, row.nb] for row in pixels_by_species],
+                },
             })
 
         return super().get(request, *args, **kwargs)
