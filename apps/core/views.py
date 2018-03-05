1533230df445ff3286d0263df9d2dc2fc22a5db7
thomasdenecker <thomas.denecker@gmail.com>
2018-03-05 21:53:52 +0100

    New information : number of Pixelsets

--- apps/core/views.py
+++ apps/core/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 
 
 from apps.core.models import Pixel
+from apps.core.models import PixelSet
 
 
 class HomeView(TemplateView):
@@ -66,6 +67,8 @@ class HomeView(TemplateView):
                 ' GROUP BY core_omicsarea.name'
             ))
 
+            number_of_pixel_sets = PixelSet.objects.count()
+
             return render(request, 'core/home-authenticated.html', {
                 'pixels_by_species': {
                     'title': _('Pixels by Species'),
@@ -90,6 +93,7 @@ class HomeView(TemplateView):
                              pixels_by_omics_area],
                 },
 
+                'number_of_pixel_sets': number_of_pixel_sets,
 
             })
 
