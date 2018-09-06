6eb0e2540a33df504497890c66ea73391b805103
Julien Maupetit <julien@maupetit.net>
2018-09-06 22:34:00 +0200

    Add pytest sugar script using test environment
    This script is intended to get used in development.

--- bin/config.sh
+++ bin/config.sh
@@ -7,3 +7,7 @@ REPO_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd)"
 function docker_compose() {
     docker-compose -p pixel-dev -f "$REPO_DIR/docker-compose.yml" "$@"
 }
+
+function docker_compose_test() {
+    docker-compose -p pixel-test -f "$REPO_DIR/docker-compose.test.yml" "$@"
+}
